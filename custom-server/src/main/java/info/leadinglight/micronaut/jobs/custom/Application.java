a628c2d30c740ef342674a47d45d591dcc31d1d0
Gerald Boersma <gerald.boersma@icsynergy.com>
2020-04-18 09:30:04 -0700

    Custom server that builds without warnings.

--- custom-server/src/main/java/info/leadinglight/micronaut/jobs/custom/Application.java
+++
@@ -1,9 +0,0 @@
-package info.leadinglight.micronaut.jobs.custom;
-
-import io.micronaut.runtime.Micronaut;
-
-public class Application {
-        public static void main(String[] args) {
-            Micronaut.run(Application.class, args);
-        }
-}
