76d311064a1ded6b740552e3657b6073c8e88951
Gerald Boersma <gerald.boersma@icsynergy.com>
2020-04-22 23:31:28 -0700

    Default properties.

--- job-scheduler/src/main/java/info/leadinglight/mjob/scheduler/QuartzPropertiesConverter.java
+++ job-scheduler/src/main/java/info/leadinglight/mjob/scheduler/QuartzPropertiesConverter.java
@@ -5,15 +5,15 @@ import io.micronaut.core.naming.NameUtils;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 
-public class PropertiesConverter {
-    // TODO Need to check for other Quartz properties that are not in standard form.
-    // TODO Specify defaults.
-    // TODO Automatically map the datasource properties.
+public class QuartzPropertiesConverter {
     public Properties getPropertiesFromConfigMap(Map<String,Object> configMap) {
         Map<String,String> flattenedConfigMap = new HashMap<>();
         flattenConfigMap(flattenedConfigMap, null, configMap);
         Properties properties = convertPropertyNames(flattenedConfigMap);
+        addDefaultProperties(properties);
+        mapOtherProperties(properties);
         return properties;
     }
 
@@ -41,11 +41,27 @@ public class PropertiesConverter {
     }
 
     private String fixKey(String key) {
+        // TODO Need to check for other Quartz properties that are not in standard form.
         String correctKey = NameUtils.camelCase(key);
         if (correctKey.equals("org.quartz.dataSource.quartzDataSource.url")) {
             return "org.quartz.dataSource.quartzDataSource.URL";
         } else {
             return correctKey;
         }
+    }
+
+    private void addDefaultProperties(Properties properties) {
+        // TODO Other defaults.
+        Set<String> names = properties.stringPropertyNames();
+        // Default value for jobFactory class must be the Micronaut Job Factory.
+        String name = "org.quartz.scheduler.jobFactory.class";
+        if (!names.contains(name)) {
+            properties.setProperty(name, "info.leadinglight.mjob.scheduler.MicronautJobFactory");
+        }
+    }
+
+    private void mapOtherProperties(Properties properties) {
+        // TODO Automatically map the datasource properties from a datasource with the same name.
+        //  Pick up the name of the jobStore.dataSource property.
     }
 }
--- job-scheduler/src/main/java/info/leadinglight/mjob/scheduler/QuartzSchedulerFactory.java
+++ job-scheduler/src/main/java/info/leadinglight/mjob/scheduler/QuartzSchedulerFactory.java
@@ -1,26 +1,22 @@
 package info.leadinglight.mjob.scheduler;
 
-import io.micronaut.core.naming.NameUtils;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.SchedulerFactory;
-import org.quartz.impl.StdScheduler;
 import org.quartz.impl.StdSchedulerFactory;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
-import java.util.PropertyPermission;
 
 @Singleton
 public class QuartzSchedulerFactory {
     public Scheduler getScheduler() {
         try {
             Map<String,Object> quartzConfig = jobConfig.getQuartz();
-            PropertiesConverter propertiesConverter = new PropertiesConverter();
-            Properties properties = propertiesConverter.getPropertiesFromConfigMap(quartzConfig);
+            QuartzPropertiesConverter quartzPropertiesConverter = new QuartzPropertiesConverter();
+            Properties properties = quartzPropertiesConverter.getPropertiesFromConfigMap(quartzConfig);
             SchedulerFactory factory = new StdSchedulerFactory(properties);
             Scheduler scheduler = factory.getScheduler();
             return scheduler;
